525b09c84d47e8d3852b6dc5d2838cca8c1aaf85
gq <guanqinghz@163.com>
2019-12-16 11:05:17 +0800

    js 基础面试,运行环境

---
+++ html/20191216/index.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function () {
+    // 页面的全部资源加载完才会执行，包括图片、视频等
+})
+document.addEventListener('DOMContentLoaded', function () {
+    // DOM 渲染完即可执行，此时图片、视频还可能没有加载完
+})
