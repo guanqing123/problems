950ed07774a37b0b46264afb24408c353387fd85
gq <guanqinghz@163.com>
2019-12-21 22:24:28 +0800

    自定义修改weui.js

--- WeuiUploader/components/WeuiUploader.js
+++ WeuiUploader/components/WeuiUploader.js
@@ -9,7 +9,7 @@ define(['vue'], function (Vue) {
                     <div class="weui-uploader__bd">
                         <ul class="weui-uploader__files"></ul>
                         <div class="weui-uploader__input-box">
-                            <input class="weui-uploader__input" type="file" accept="image/*" capture="camera" multiple="" />
+                            <input ref="file" class="weui-uploader__input" type="file" accept="image/*" capture="camera" multiple="" />
                         </div>
                     </div>
                 </div>
@@ -34,6 +34,46 @@ define(['vue'], function (Vue) {
                     return []
                 }
             }
+        },
+        data: function () {
+            return {
+                _id: 0
+            }
+        },
+        methods: {
+            // 设置上传
+            setUploadFile: function (file) {
+                file.url =
+            }
+        },
+        mounted: function () {
+            var URL = window.URL || window.webkitURL || window.mozURL,  self = this
+            self.$refs['file'].addEventListener('change', function(e) {
+                var files = e.target.files
+
+                if (files.length === 0)
+                    return;
+
+                if (self.options.compress === false && self.options.type == 'file') {
+                    // 以原文件方式上传
+                    Array.prototype.forEach.call(files, function (file) {
+                        file.id = ++self._id;
+
+                        if (self.options.onBeforeQueued(file, files) === false) return;
+
+                    })
+                } else {
+                    // base64上传 和 压缩上传
+                    Array.prototype.forEach.call(files, function (file) {
+                        file.id = ++self._id;
+
+                        if (self.options.onBeforeQueued(file, files) === false) return;
+
+
+                    })
+                }
+
+            })
         }
     })
 })
