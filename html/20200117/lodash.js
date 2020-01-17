d8e23a26f475568dafb2ea58fc4c7fcf02c337d2
gq <guanqinghz@163.com>
2020-01-17 16:40:20 +0800

    测试 fujian 上传

---
+++ html/20200117/lodash.js
@@ -0,0 +1,5 @@
+var a = {
+    a: '1',
+    b: '3'
+}
+console.log(JSON.stringify(_.assign(a, {c:'5', d:'7'})))
---
+++ html/20200117/zhichumingxi.js
@@ -0,0 +1,384 @@
+var zcbzz =
+{
+    "ZhiChuXX":[
+        {
+            "ZhiChuMXBH":"ZCMX201707110242",
+            "FeiYongMX":[
+                {
+                    "MingXiBH":"FYMX201809040001",
+                    "MingXiZ":[
+                        "FZXXZ201705230002",
+                        "FZXXZ201705230002"
+                    ]
+                },
+                {
+                    "MingXiBH":"FYMX201809040002",
+                    "MingXiZ":[
+                        "FZXXZ201705230005",
+                        "FZXXZ201705230005"
+                    ]
+                },
+                {
+                    "MingXiBH":"FYMX201809040003",
+                    "MingXiZ":[
+                        "1",
+                        "1"
+                    ]
+                }
+            ]
+        }
+    ]
+}
+var zhichumingxi = [
+    {
+        "BianHao": "ZCMX201910280001",
+        "IsShowBZZ": "false",
+        "JinE": "0",
+        "MingCheng": "城市交通费",
+        "IsOcr": "1",
+        "FeiYongMX": [
+            [
+                {
+                    "BianHao": "FYMX201911190015",
+                    "Options": [],
+                    "Type": "2",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "name",
+                    "FeiYongMXMC": "乘车人"
+                },
+                {
+                    "BianHao": "FYMX201911190016",
+                    "Options": [],
+                    "Type": "2",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "startStation",
+                    "FeiYongMXMC": "出发站"
+                },
+                {
+                    "BianHao": "FYMX201911190017",
+                    "Options": [],
+                    "Type": "2",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "arriveStation",
+                    "FeiYongMXMC": "到达站"
+                },
+                {
+                    "BianHao": "FYMX201911190018",
+                    "Options": [],
+                    "Type": "3",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "date",
+                    "FeiYongMXMC": "出发日期"
+                },
+                {
+                    "BianHao": "FYMX201911190019",
+                    "Options": [],
+                    "Type": "2",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "seatCategory",
+                    "FeiYongMXMC": "座位类型"
+                },
+                {
+                    "BianHao": "FYMX201911190020",
+                    "Options": [],
+                    "Type": "1",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "ticketPrice",
+                    "FeiYongMXMC": "票价"
+                },
+                {
+                    "BianHao": "FYMX201911190021",
+                    "Options": [],
+                    "Type": "2",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "trainNum",
+                    "FeiYongMXMC": "车次"
+                },
+                {
+                    "Type": -1,
+                    "OcrCode": "FuJianXX",
+                    "FeiYongMXMC": "附件",
+                    "FeiYongMXZ": "",
+                    "FuJianMC": "",
+                    "FuJianKey": ""
+                }
+            ]
+        ]
+    },
+    {
+        "BianHao": "ZCMX201910280002",
+        "IsShowBZZ": "false",
+        "JinE": "",
+        "MingCheng": "差旅费",
+        "IsOcr": "2",
+        "FeiYongMX": [
+            [
+                {
+                    "BianHao": "FYMX201911190022",
+                    "Options": [],
+                    "Type": "2",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "sellerId",
+                    "FeiYongMXMC": "销售方纳税人识别号",
+                    "FeiYongMXZ": "91110108723969982M"
+                },
+                {
+                    "BianHao": "FYMX201911190023",
+                    "Options": [],
+                    "Type": "2",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "sellerBank",
+                    "FeiYongMXMC": "销售方开户行及账号",
+                    "FeiYongMXZ": "中国工商银行股份有限公司北京海淀支行营业部020004961920005330B"
+                },
+                {
+                    "BianHao": "FYMX201911190024",
+                    "Options": [],
+                    "Type": "2",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "sellerName",
+                    "FeiYongMXMC": "销售方名称",
+                    "FeiYongMXZ": "北京中科三方网络技术有限公司"
+                },
+                {
+                    "BianHao": "FYMX201911190025",
+                    "Options": [],
+                    "Type": "2",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "buyerId",
+                    "FeiYongMXMC": "购买方纳税人识别号",
+                    "FeiYongMXZ": "91330100143054524X"
+                },
+                {
+                    "BianHao": "FYMX201911190026",
+                    "Options": [],
+                    "Type": "2",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "buyerName",
+                    "FeiYongMXMC": "购买方名称",
+                    "FeiYongMXZ": "杭州鸿雁电器有限公司"
+                },
+                {
+                    "BianHao": "FYMX201911190027",
+                    "Options": [],
+                    "Type": "2",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "del",
+                    "FeiYongMXMC": "是否作废"
+                },
+                {
+                    "BianHao": "FYMX201911190028",
+                    "Options": [],
+                    "Type": "3",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "issueDate",
+                    "FeiYongMXMC": "开票日期",
+                    "FeiYongMXZ": "2019-07-24"
+                },
+                {
+                    "BianHao": "FYMX201911190029",
+                    "Options": [],
+                    "Type": "1",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "subtotalAmount",
+                    "FeiYongMXMC": "合计金额",
+                    "FeiYongMXZ": "2641.51"
+                },
+                {
+                    "BianHao": "FYMX201911190030",
+                    "Options": [],
+                    "Type": "1",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "subtotalTax",
+                    "FeiYongMXMC": "合计税额",
+                    "FeiYongMXZ": "158.49"
+                },
+                {
+                    "BianHao": "FYMX201911190031",
+                    "Options": [],
+                    "Type": "2",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "type",
+                    "FeiYongMXMC": "发票类型",
+                    "FeiYongMXZ": "专用发票"
+                },
+                {
+                    "BianHao": "FYMX201911190032",
+                    "Options": [],
+                    "Type": "2",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "checkCode",
+                    "FeiYongMXMC": "发票校验码",
+                    "FeiYongMXZ": ""
+                },
+                {
+                    "BianHao": "FYMX201911190033",
+                    "Options": [],
+                    "Type": "2",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "number",
+                    "FeiYongMXMC": "发票号",
+                    "FeiYongMXZ": "02237701"
+                },
+                {
+                    "BianHao": "FYMX201911190034",
+                    "Options": [],
+                    "Type": "2",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "code",
+                    "FeiYongMXMC": "发票代码",
+                    "FeiYongMXZ": "1100191130"
+                },
+                {
+                    "BianHao": "FYMX201911190035",
+                    "Options": [],
+                    "Type": "1",
+                    "BiaoZhunFZXH": "",
+                    "OcrCode": "total",
+                    "FeiYongMXMC": "价税合计",
+                    "FeiYongMXZ": "贰仟捌佰圆整"
+                },
+                {
+                    "Type": -1,
+                    "OcrCode": "FuJianXX",
+                    "FeiYongMXMC": "附件",
+                    "FeiYongMXZ": "http://119.18.195.153:8089/bpm/image//upload/FJ200117105600048.jpg",
+                    "FuJianMC": "FJ200117105600048.jpg",
+                    "FuJianKey": "b2966f058c736f40897bf21e92d97ece",
+                    "api": "vat_invoice",
+                    "FaPiaoDM": "1100191130",
+                    "FaPiaoHM": "02237701",
+                    "KaiJuJE": "2800",
+                    "KaiPiaoRQ": "2019-07-24",
+                    "JiaoYanM": ""
+                }
+            ]
+        ]
+    },
+    {
+        "BianHao": "ZCMX201812240001",
+        "IsShowBZZ": "true",
+        "JinE": "0",
+        "MingCheng": "工伤险",
+        "FeiYongMX": [
+            [
+                {
+                    "BianHao": "FYMX201912090016",
+                    "Options": [
+                        {
+                            "Value": "FZXXZ201912060004",
+                            "Text": "12"
+                        },
+                        {
+                            "Value": "FZXXZ201912060005",
+                            "Text": "121"
+                        },
+                        {
+                            "Value": "FZXXZ201912060006",
+                            "Text": "1212"
+                        }
+                    ],
+                    "Type": 4,
+                    "BiaoZhunFZXH": "1",
+                    "FeiYongMXMC": "级别"
+                },
+                {
+                    "BianHao": "FYMX201912090017",
+                    "Options": [
+                        {
+                            "Value": "FZXXZ201912060001",
+                            "Text": "1"
+                        },
+                        {
+                            "Value": "FZXXZ201912060003",
+                            "Text": "12"
+                        }
+                    ],
+                    "Type": 4,
+                    "BiaoZhunFZXH": "2",
+                    "FeiYongMXMC": "差旅费"
+                },
+                {
+                    "BianHao": "FYMX201912090018",
+                    "Options": [
+                        {
+                            "Value": "FZXXZ201912060002",
+                            "Text": "基本支出"
+                        }
+                    ],
+                    "Type": 4,
+                    "BiaoZhunFZXH": "3",
+                    "FeiYongMXMC": "基本支出"
+                },
+                {
+                    "BianHao": "FYMX201912090019",
+                    "Options": [],
+                    "Type": "1",
+                    "BiaoZhunFZXH": "",
+                    "FeiYongMXMC": "a"
+                },
+                {
+                    "BianHao": "FYMX201912090020",
+                    "Options": [],
+                    "Type": "1",
+                    "BiaoZhunFZXH": "",
+                    "FeiYongMXMC": "v"
+                },
+                {
+                    "Type": 0,
+                    "FeiYongMXMC": "标准费用",
+                    "FeiYongMXZ": "__vue_devtool_undefined__"
+                }
+            ]
+        ]
+    },
+    {
+        "BianHao": "ZCMX201812240002",
+        "IsShowBZZ": "false",
+        "JinE": "0.00",
+        "MingCheng": "失业险",
+        "FeiYongMX": []
+    },
+    {
+        "BianHao": "ZCMX201812240003",
+        "IsShowBZZ": "false",
+        "JinE": "0",
+        "MingCheng": "住房公积金",
+        "FeiYongMX": [
+            [
+                {
+                    "BianHao": "FYMX201912090003",
+                    "Options": [],
+                    "Type": "1",
+                    "BiaoZhunFZXH": "",
+                    "FeiYongMXMC": "a"
+                },
+                {
+                    "BianHao": "FYMX201912090004",
+                    "Options": [],
+                    "Type": "1",
+                    "BiaoZhunFZXH": "",
+                    "FeiYongMXMC": "b"
+                }
+            ]
+        ]
+    }
+]
+
+_.forEach(zhichumingxi, function (v, i) {
+    var c = _.flattenDepth(v.FeiYongMX, 1);
+    var tempFeiYongMX = [];
+    // console.log('BianHao>' + JSON.stringify(_.groupBy(c, 'BianHao')));
+
+    _.forEach(_.groupBy(c, 'BianHao'), function (x, y) {
+        if (y != 'undefined') {
+            tempFeiYongMX.push({
+                "MingXiBH":y,
+                "MingXiZ":_.map(x, function (v) {
+
+                })
+            })
+        }
+    });
+    console.log(JSON.stringify(tempFeiYongMX))
+});
